a9f1fa223c3af951a83805ac206526eaed80f83a
Dylan Quenneville <dquenne@gmail.com>
2023-06-08 13:19:39 -0700

    Just use `memcpy` for loading presets

--- Patch.cpp
+++ Patch.cpp
@@ -22,39 +22,8 @@ void PsgPatchState::initialize() {
   frequency_lfo_state.initialize();
 }
 
-void applyPresetEnvelope(EnvelopeShape *target, const EnvelopeShape *preset) {
-  target->steps[0] = preset->steps[0];
-  target->steps[1] = preset->steps[1];
-  target->steps[2] = preset->steps[2];
-  target->steps[3] = preset->steps[3];
-  target->steps[4] = preset->steps[4];
-  target->loop_enable = preset->loop_enable;
-  target->loop_after_step = preset->loop_after_step;
-  target->loop_to_step = preset->loop_to_step;
-  target->on_off_jump_to_step = preset->on_off_jump_to_step;
-  target->end_after_step = preset->end_after_step;
-}
-
-void applyPresetLfo(Lfo *target, const Lfo *preset) {
-  target->depth = preset->depth;
-  target->waveform = preset->waveform;
-  target->wavelength = preset->wavelength;
-  target->start_delay_ticks = preset->start_delay_ticks;
-  target->off_ticks = preset->off_ticks;
-}
-
 void applyPsgPreset(PsgPatch *target, const PsgPatch *preset) {
-  applyPresetEnvelope(&target->amplitude_envelope, &preset->amplitude_envelope);
-  applyPresetEnvelope(&target->frequency_envelope, &preset->frequency_envelope);
-  applyPresetLfo(&target->amplitude_lfo, &preset->amplitude_lfo);
-  applyPresetLfo(&target->frequency_lfo, &preset->frequency_lfo);
-  target->velocity_config.velocity_center =
-      preset->velocity_config.velocity_center;
-  target->velocity_config.interval = preset->velocity_config.interval;
-  target->delay_config.enable = preset->delay_config.enable;
-  target->delay_config.delay_ticks = preset->delay_config.delay_ticks;
-  target->delay_config.detune_cents = preset->delay_config.detune_cents;
-  target->delay_config.attenuation = preset->delay_config.attenuation;
+  memcpy(target, preset, sizeof(*preset));
 }
 
 void PsgPatchState::setPatch(const PsgPatch *patch, bool is_delay) {
@@ -153,40 +122,6 @@ unsigned FmPatchState::getOperatorLevel(unsigned op) {
   return velocity_scaled_level;
 }
 
-void applyPresetOperator(FmOperator *target, const FmOperator *preset) {
-  target->attack_rate = preset->attack_rate;
-  target->decay_rate = preset->decay_rate;
-  target->sustain_rate = preset->sustain_rate;
-  target->release_rate = preset->release_rate;
-  target->sustain_level = preset->sustain_level;
-  target->total_level = preset->total_level;
-  target->key_scale = preset->key_scale;
-  target->multiple = preset->multiple;
-  target->detune = preset->detune;
-  target->lfo_amplitude_enable = preset->lfo_amplitude_enable;
-}
-
-void applyPresetFMCoreParameters(FmParameters *target,
-                                 const FmParameters *preset) {
-  applyPresetOperator(&target->operators[0], &preset->operators[0]);
-  applyPresetOperator(&target->operators[1], &preset->operators[1]);
-  applyPresetOperator(&target->operators[2], &preset->operators[2]);
-  applyPresetOperator(&target->operators[3], &preset->operators[3]);
-  target->panning = preset->panning;
-  target->feedback = preset->feedback;
-  target->algorithm = preset->algorithm;
-  target->lfo_amplitude_sensitivity = preset->lfo_amplitude_sensitivity;
-  target->lfo_frequency_sensitivity = preset->lfo_frequency_sensitivity;
-}
-
 void applyFmPreset(FmPatch *target, const FmPatch *preset) {
-  memcpy(&target->velocity_level_scaling, &preset->velocity_level_scaling,
-         sizeof(preset->velocity_level_scaling));
-  applyPresetLfo(&target->frequency_lfo, &preset->frequency_lfo);
-  applyPresetFMCoreParameters(&target->core_parameters,
-                              &preset->core_parameters);
-  target->delay_config.enable = preset->delay_config.enable;
-  target->delay_config.delay_ticks = preset->delay_config.delay_ticks;
-  target->delay_config.detune_cents = preset->delay_config.detune_cents;
-  target->delay_config.attenuation = preset->delay_config.attenuation;
+  memcpy(target, preset, sizeof(*preset));
 }
